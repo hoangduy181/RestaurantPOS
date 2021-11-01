b55fa80bf6b0c130d5228d117c2b81f7d29cef09
hoangduy181 <hoang.duy.181001@gmail.com>
2021-11-01 09:57:13 +0700

    Duy test

--- ResPos/reactapp/src/App.js
+++ ResPos/reactapp/src/App.js
@@ -1,27 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './ComponentJS/header'
+import Logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      
-      <body>
-
-      </body>
+      <Header/>
+      <img scr = {Logo} alt = "react-logo"  ></img>
     </div>
   );
 }
