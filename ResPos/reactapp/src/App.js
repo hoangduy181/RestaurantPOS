b5a7099c10d782fac4b19a2566f68f9229c1f2ba
Perovenus509 <89351882+Perovenus509@users.noreply.github.com>
2021-11-23 13:33:01 +0700

    Change app.js

--- ResPos/reactapp/src/App.js
+++ ResPos/reactapp/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Menu from './ComponentJS/Order/Menu';
-import Cart from './ComponentJS/Order/Cart'
+import OrderScreen from './ComponentJS/OrderComponent/Orderjs/OrderScreen';
+import CartScreen from './ComponentJS/OrderComponent/Orderjs/CartScreen';
 import { BrowserRouter as Router, Routes, Route,Link,useRoutes } from "react-router-dom"
 function App() {
   let routes = useRoutes([
-    { path: "/", element: <Menu /> },
-    { path: "/Cart", element: <Cart /> },
+    { path: "/", element: <OrderScreen /> },
+    { path: "/Cart", element: <CartScreen /> },
   ]);
   return (
     routes
